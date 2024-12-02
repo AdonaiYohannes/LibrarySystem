6e73170cd3e024cc6e56293989ea6991d0a732c7
AdonaiYohannes <adonai.yohannes@gmail.com>
2024-12-03 00:12:21 +0100

    Project start ER-Molel/Relationship

--- obj/Debug/net8.0/Bibliotek.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bibliotek.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliotek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+255034ac1e142190f10889dc33ef6b7a6b5e4afa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliotek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliotek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
